f713cfe5b90cc72a11b6080e054751fa241b93c8
Emmettscode <109743658+Emmettscode@users.noreply.github.com>
2022-07-21 10:57:49 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title> ERA-Emmett </title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to ERA-Emmett!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          If You See Emmett In real life, Ask him if you can join our team! <code>pages/index.js</code>
         </p>
       </main>
 
